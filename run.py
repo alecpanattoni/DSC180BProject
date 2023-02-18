ee1791146aa4d6bcd392b3dde05acda1f5afd605
giwi2123 <gykim@ucsd.edu>
2023-02-17 22:06:49 -0800

    added accuracy visualizations '

--- run.py
+++ run.py
@@ -130,21 +130,9 @@ nmar = nmar.dropna(subset = 'substantiated')
 cat = ["complainant_ethnicity", "complainant_age_incident", "allegation", "contact_reason"]
 
 
-<<<<<<< HEAD
-# In[17]:
-
-
-acc = []
-
-
 # ### Calculating fairnes notions for No Missingness At All
 
 # In[18]:
-=======
-# ### Calculating fairnes notions for No Missingness At All
-
-# In[17]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 #storing fairness notions for no missingness
@@ -153,7 +141,6 @@ no_missing = model_perform.model(train, test, cat)
 no_missing_fairness.append(no_missing)
 
 
-<<<<<<< HEAD
 # In[19]:
 
 
@@ -163,21 +150,12 @@ no_missing
 # ### Fairness notions for NMAR
 
 # In[20]:
-=======
-# ### Fairness notions for NMAR
-
-# In[18]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 train_nmar, test_nmar = train_test_split(nmar, test_size=0.2)
 
 
-<<<<<<< HEAD
 # In[21]:
-=======
-# In[19]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 nmar_fairness = []
@@ -187,21 +165,13 @@ nmar_fairness.append(nmar_model)
 
 # ### Fairness notions for MCAR
 
-<<<<<<< HEAD
 # In[22]:
-=======
-# In[20]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 train_mcar, test_mcar = train_test_split(mcar, test_size=0.2)
 
 
-<<<<<<< HEAD
 # In[23]:
-=======
-# In[21]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 mcar_fairness = []
@@ -211,21 +181,13 @@ mcar_fairness.append(mcar_model)
 
 # ### Fairness notions for MAR
 
-<<<<<<< HEAD
 # In[24]:
-=======
-# In[22]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 train_mar, test_mar = train_test_split(mar, test_size=0.2)
 
 
-<<<<<<< HEAD
 # In[25]:
-=======
-# In[23]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 mar_fairness = []
@@ -233,7 +195,6 @@ mar_model = model_perform.model_missing(train_mar, test_mar, cat)
 mar_fairness.append(mar_model)
 
 
-<<<<<<< HEAD
 # In[26]:
 
 
@@ -242,44 +203,23 @@ acc = [no_missing_fairness[0][0],nmar_fairness[0][0],mcar_fairness[0][0],mar_fai
 par= [no_missing_fairness[0][1],nmar_fairness[0][1],mcar_fairness[0][1],mar_fairness[0][1]]
 odds= [no_missing_fairness[0][2],nmar_fairness[0][2],mcar_fairness[0][2],mar_fairness[0][2]]
 opp = [no_missing_fairness[0][3],nmar_fairness[0][3],mcar_fairness[0][3],mar_fairness[0][3]]
-=======
-# In[24]:
-
-
-#put our fairness statistics into arrays for future usage
-par= [no_missing_fairness[0][0],nmar_fairness[0][0],mcar_fairness[0][0],mar_fairness[0][0]]
-odds= [no_missing_fairness[0][1],nmar_fairness[0][1],mcar_fairness[0][1],mar_fairness[0][1]]
-opp = [no_missing_fairness[0][2],nmar_fairness[0][2],mcar_fairness[0][2],mar_fairness[0][2]]
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 # ## Visualizing Our Results
 
-<<<<<<< HEAD
 # In[27]:
-=======
-# In[25]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 import matplotlib.pyplot as plt
 
 
-<<<<<<< HEAD
 # In[28]:
-=======
-# In[26]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 labels = ['No Missingess,', 'NMAR', 'MCAR', 'MAR']
 
 
-<<<<<<< HEAD
 # In[29]:
-=======
-# In[27]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 plt.figure(figsize = (20, 10))
@@ -290,11 +230,7 @@ plt.ylim(min(par) - 0.1, max(par) + 0.1)
 plt.plot(labels, par, marker='.', markersize = 20)
 
 
-<<<<<<< HEAD
 # In[30]:
-=======
-# In[28]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 plt.figure(figsize = (20, 10))
@@ -305,11 +241,7 @@ plt.ylim(min(odds)-0.1,max(odds) + 0.1)
 plt.plot(labels, odds, marker='.', markersize = 20)
 
 
-<<<<<<< HEAD
 # In[31]:
-=======
-# In[29]:
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 
 
 plt.figure(figsize = (20, 10))
@@ -320,8 +252,7 @@ plt.ylim(min(opp)-0.1,max(opp) + 0.1)
 plt.plot(labels, opp, marker='.', markersize = 20)
 
 
-<<<<<<< HEAD
-# In[33]:
+# In[32]:
 
 
 plt.figure(figsize = (20, 10))
@@ -332,8 +263,6 @@ plt.ylim(min(acc)-0.1,max(acc) + 0.1)
 plt.plot(labels, acc, marker='.', markersize = 20)
 
 
-=======
->>>>>>> ea230a069629d60e2d13335a75246535f4f5af9d
 # In[ ]:
 
 
